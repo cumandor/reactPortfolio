3f354de0841231908b1f4a2d2cfb89d23be3ac34
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-12 07:08:25 +0200

    review contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,22 +1,20 @@
-import { useForm } from 'react-hook-form';
-import './contact.css';
+import { useForm } from "react-hook-form";
+import "./contact.css";
 
 function Contact() {
-const {register, handleSubmit} = useForm()
-const onSubmit = (data) => {
-    alert(`your name ${data.name}`)
-}
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    alert(`your name ${data.name}`);
+  };
 
   return (
     <section className="contact">
       <div className="container">
         <div className="contact__inner">
-      
-        <form onSubmit={handleSubmit(onSubmit)}>
-
-                <input {...register('name')} type="text" />
-            </form>
-
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("name")} type="text" />
+          </form>
         </div>
       </div>
     </section>
